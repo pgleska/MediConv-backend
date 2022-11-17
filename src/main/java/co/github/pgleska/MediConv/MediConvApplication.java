d86c1a2de984f9ccb12199632d87c453ec7082ff
Piotr Gleska <piotr.gleska@wp.pl>
2022-11-17 16:12:13 +0100

    initial commit

---
+++ src/main/java/co/github/pgleska/MediConv/MediConvApplication.java
@@ -0,0 +1,13 @@
+package co.github.pgleska.MediConv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediConvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediConvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/github/pgleska/MediConv/MediConvApplicationTests.java
@@ -0,0 +1,13 @@
+package co.github.pgleska.MediConv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediConvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
